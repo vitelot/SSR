59ba91c1cdafce119efdf98fe4aaf56987060a37
Vito DP Servedio <v.servedio@gmail.com>
2017-11-23 17:42:23 +0100

    first complete version

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+main();
+
 function main() {
 
   ///////////////////////////
@@ -5,13 +7,17 @@ function main() {
     d3.select("#pauseB")
       .on("click", pauseSym);
 
-    // d3.select("#restartB")
-    //   .on("click", restartSym);
+    d3.select("#muteB")
+      .on("click", muteSym);
+
+    d3.select("#restartB")
+      .on("click", restartSym);
   ////////////////////
 
   var runningFlag = true;
   var timer;
 
+///////// for the simulation ///////////
   var svgs = d3.select("#simulation svg"),
       margin = {top: 20, right: 20, bottom: 20, left: 20},
       width = +svgs.attr("width") - margin.left - margin.right,
@@ -22,9 +28,23 @@ function main() {
 
   var g = svgs.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+////////////////////////////////////////
+
+///////// for the histogramm ///////////
+  var svgi = d3.select("#info svg"),
+      margini = {top: 20, right: 20, bottom: 20, left: 50},
+      widthi = +svgi.attr("width") - margini.left - margini.right,
+      heighti = +svgi.attr("height") - margini.top - margini.bottom;
+
+  var xi = d3.scaleLog().base(10).range([widthi, 0]),
+      yi = d3.scaleLog().base(10).range([heighti, 0]);
+
+  var gi = svgi.append("g")
+      .attr("transform", "translate(" + margini.left + "," + margini.top + ")");
+////////////////////////////////////////
 
   var N = 50;
-  var rate = 200;
+  var rate = 300;
   var barcol0 = "steelblue";
   var barcol1 = "red";
   var bars = [];
@@ -48,7 +68,7 @@ function main() {
 
   for(i=0; i<N; i++) {
     bars[i] = 1-i/N;
-    histo[i] = 0;
+    histo[i] = 1e-6;
   }
 
   x.domain(bars.map(function(d,i) { return i; }));
@@ -65,11 +85,54 @@ function main() {
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return height - y(d); });
 
+  xi.domain([1, N]);
+  yi.domain([1, 1000]);
+
+  gi.selectAll(".point")
+    .data(histo)
+    .enter().append("circle")
+      .attr("class", "point")
+      .attr("id", function(d,i) { return "point-"+i;})
+      .attr("cx", function(d,i) { return xi(i+1e-6); })
+      .attr("cy", function(d) { return yi(d); })
+      .attr("r", "8")
+      .attr("fill", "white")
+      .attr("stroke", "black")
+      // .attr("width", x.bandwidth())
+      // .attr("height", function(d) { return height - y(d); });
+
+  gi.append("g")
+  .attr("class", "axis axis--x")
+  .attr("transform", "translate(0," + yi(1) + ")")
+  .call(d3.axisBottom(xi).ticks(10,",.0f"));
+
+  gi.append("text")
+  .attr("id", "label--x")
+  .attr("transform", "translate(" + (widthi-100) + "," + (heighti+margini.bottom) + ")")
+  .text("Index");
+
+
+  gi.append("g")
+  .attr("class", "axis axis--y")
+  .attr("transform", "translate("+ xi(N) +","+ yi(1000) + ")")
+  .call(d3.axisLeft(yi).ticks(10,",.0f"));
+
+  gi.append("text")
+  .attr("id", "label--y")
+  .attr("transform", "translate(" + (-25) + "," + (heighti/2+15) + ")rotate(-90)")
+  // .attr("transform", "rotate(-90)")
+  .text("Count");
+
   var pos;
   start();
 
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+
   function start() {
 
+    runningFlag = true;
     pos=0;
 
     d3.selectAll(".bar")
@@ -81,15 +144,15 @@ function main() {
   function run() {
     let r = pos + 1 + Math.random()*(N-pos-1) | 0; // integer from pos+1 to N-1
     pos = r;
-    histo[pos]++;
+    histo[pos] += 1;
 
     let sound = notes[(N-pos)%7];
-    let octave = 2+Math.floor((N-pos)/14);
-    piano.play(sound, octave, .5*rate/1000); // plays C4 for 2s using the 'piano' sound profile
+    let octave = 2+Math.floor((N-pos)/13);
+    piano.play(sound, octave, .7*rate/1000); // plays C4 for 2s using the 'piano' sound profile
 
     for( i=0; i<r; i++) {
       d3.select("#bar-"+i)
-        .style("opacity", "0.5");
+        .style("opacity", "0.3");
     }
 
     d3.select("#bar-"+r)
@@ -97,6 +160,11 @@ function main() {
       .transition()
       .duration(0.7*rate)
       .style("fill", barcol0);
+
+    d3.select("#point-"+r)
+      .transition()
+      .duration(0.7*rate)
+      .attr("cy", yi(histo[r]))
 
     if(pos === N-1) {
       clearInterval(timer);
@@ -108,10 +176,38 @@ function main() {
     if (runningFlag) {
       clearInterval(timer);
       runningFlag = false;
+      d3.select("#bar-"+pos)
+        .transition()
+        .duration(100)
+        .style("fill", barcol1);
     } else {
       timer = setInterval(run, rate);
       runningFlag = true;
+      d3.select("#bar-"+pos)
+        .style("fill", barcol0);
     }
+  }
+
+  function restartSym() {
+      clearInterval(timer);
+      for(i=0; i<N; i++) histo[i] = 1e-6;
+
+      d3.selectAll(".bar")
+        .transition()
+        .duration(1000)
+        .style("fill", barcol0)
+        .style("opacity", "1");
+
+      gi.selectAll(".point")
+          .transition()
+          .duration(1000)
+          .attr("cy", yi(1e-6));
+
+      setTimeout(start, 1100);
+  }
+
+  function muteSym() {
+    Synth.getVolume()>1e-3? Synth.setVolume(0.0): Synth.setVolume(1.0);
   }
 
 }
